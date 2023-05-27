13ad2de383413d36f9d5ed702fbc33bd686f7b29
imortaltatsu <adityaberry1234@gmail.com>
2023-05-27 22:14:40 +0530

    test

--- api.py
+++ api.py
@@ -1,2 +1,16 @@
 import flask
 import flask_cors
+import pandas as pd
+import requests
+import time
+import json
+from bs4 import BeautifulSoup
+
+app = flask.Flask(__name__)
+flask_cors.CORS(app)
+
+@app.route('/api/v1/surebets', methods=['GET'])
+def get_surebets():
+    df = 
+    return df.to_json(orient='records')
+    return df
